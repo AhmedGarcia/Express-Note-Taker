360ca83d282de8ddd4b61b6aaf0b14c2171d80f7
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-07-08 23:03:28 -0400

    changed server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Use API routes
 app.use('/api', apiRoutes);
